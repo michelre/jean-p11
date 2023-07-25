b7c5ef12413a1a2bef81b03067c9cc6b46889bb4
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-25 17:16:41 +0200

    Mobile menu JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('JS Connected yeah!');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileCloseMenuBtn = document.querySelector('.mobile-close-menu-btn');
+const mobileHeaderNav = document.querySelector('.mobile-header-nav');
+
+//Mobile menu handling
+burgerMenu.addEventListener('click', openMobileMenu);
+function openMobileMenu() {
+    mobileCloseMenuBtn.style.display = 'block';
+    burgerMenu.style.display = 'none';
+    mobileHeaderNav.style.display = 'flex'
+}
+
+mobileCloseMenuBtn.addEventListener('click', closeMobileMenu);
+function closeMobileMenu() {
+    mobileCloseMenuBtn.style.display = 'none';
+    burgerMenu.style.display = 'block';
+    mobileHeaderNav.style.display = 'none'
+}
