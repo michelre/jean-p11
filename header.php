dd7a9f135a18f3ff50ed4988e3f1c7d4a893af3a
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-25 17:05:11 +0200

    Mobile header

--- header.php
+++ header.php
@@ -12,11 +12,24 @@
 
 <body>
     <header>
-        <div class="header">
+        <div class="header desktop-header">
             <a href="<?php echo get_home_url() ?>">
-                <img class="header__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Nathalie Mota Logo" />
+                <img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Nathalie Mota Logo" />
             </a>
-            <nav>
+            <nav class="header-nav">
+                <?php wp_nav_menu(['theme_location' => 'header', 'container' => 'false']) ?>
+            </nav>
+        </div>
+
+        <div class="mobile-header">
+            <div class="mobile-header-container">
+                <a href="<?php echo get_home_url() ?>">
+                    <img class="mobile-header-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Nathalie Mota Logo" />
+                </a>
+                <img class="burger-menu" src="<?php echo get_template_directory_uri(); ?>/assets/images/burger-menu.png" alt="close" />
+                <img class="mobile-close-menu-btn" src="<?php echo get_template_directory_uri(); ?>/assets/images/close-icon.png" alt="close" />
+            </div>
+            <nav class="mobile-header-nav">
                 <?php wp_nav_menu(['theme_location' => 'header', 'container' => 'false']) ?>
             </nav>
         </div>
