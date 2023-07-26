d997f32ffcd76e03ce46af54677fd5b18508ac22
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-26 15:24:31 +0200

    Contact Modal JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,47 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const mobileCloseMenuBtn = document.querySelector('.mobile-close-menu-btn');
-const mobileHeaderNav = document.querySelector('.mobile-header-nav');
+const burgerMenu = document.querySelector(".burger-menu");
+const mobileCloseMenuBtn = document.querySelector(".mobile-close-menu-btn");
+const mobileHeaderNav = document.querySelector(".mobile-header-nav");
+const contactModal = document.querySelector(".contact-modal");
+const contactModalBtn = document.querySelector(".menu-item-11");
+const contactModalBtnMobile = document.querySelector(
+  ".mobile-header-nav .menu-item-11"
+);
+const contactModalHeader = document.querySelector(".contact-modal-header");
+const contactModalForm = document.querySelector(".wpcf7-form");
 
 //Mobile menu handling
-burgerMenu.addEventListener('click', openMobileMenu);
+burgerMenu.addEventListener("click", openMobileMenu);
 function openMobileMenu() {
-    mobileCloseMenuBtn.style.display = 'block';
-    burgerMenu.style.display = 'none';
-    mobileHeaderNav.style.display = 'flex'
+  mobileCloseMenuBtn.style.display = "block";
+  burgerMenu.style.display = "none";
+  mobileHeaderNav.style.display = "flex";
 }
 
-mobileCloseMenuBtn.addEventListener('click', closeMobileMenu);
+mobileCloseMenuBtn.addEventListener("click", closeMobileMenu);
 function closeMobileMenu() {
-    mobileCloseMenuBtn.style.display = 'none';
-    burgerMenu.style.display = 'block';
-    mobileHeaderNav.style.display = 'none'
+  mobileCloseMenuBtn.style.display = "none";
+  burgerMenu.style.display = "block";
+  mobileHeaderNav.style.display = "none";
 }
+
+//Contact modal handling
+function openContactmodal() {
+  contactModal.style.display = "flex";
+}
+contactModalBtn.addEventListener("click", openContactmodal);
+contactModalBtnMobile.addEventListener("click", openContactmodal);
+
+function closeContactmodal() {
+  contactModal.style.display = "none";
+}
+
+document.addEventListener("click", function (event) {
+  if (event.target == contactModal) {
+    closeContactmodal();
+  }
+});
+
+contactModalHeader.addEventListener("click", closeContactmodal);
+
+//Close modal on successful form submission
+document.addEventListener('wpcf7mailsent',closeContactmodal);
