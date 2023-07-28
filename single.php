d0752bdc20440e94d9fd37eadeed5b7d6d23a703
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-28 15:23:32 +0200

    Single post top section css 1/2

--- single.php
+++ single.php
@@ -18,33 +18,40 @@ while (have_posts()) : the_post(); ?>
             </div>
         </div>
         <div class="single-photo-nav-section">
-            <p class="texte">Cette photo vous intéresse ?</p>
-            <button>Contact</button>
-
-            <div class="single-photo-nav">
-                <?php
-                $prevPhoto = get_previous_post();
-                $nextPhoto = get_next_post();
-                if ($prevPhoto) : ?>
-                    <a class="prev-photo-arrow" href="<?php echo get_permalink($prevPhoto->ID); ?>">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/images/left-arrow.png'; ?>" alt="Previous Photo" />
-                    </a>
-                <?php endif; ?>
-                <?php if ($nextPhoto) : ?>
-                    <a class="next-photo-arrow" href="<?php echo get_permalink($nextPhoto->ID); ?>">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/images/right-arrow.png'; ?>" alt="Next Photo" />
-                    </a>
-                <?php endif; ?>
+            <div class="single-photo-nav-section-left">
+                <p class="texte">Cette photo vous intéresse ?</p>
+                <div class="mota-button">Contact</div>
             </div>
 
+            <div class="single-photo-nav-section-right">
+                <div class="single-nav-container" >
+                <div class="single-photo-nav-preview">
+                    <?php
+                    $prevPhoto = get_previous_post();
+                    $nextPhoto = get_next_post();
+                    if ($prevPhoto) : ?>
+                        <img class="previous-image " src="<?php echo get_the_post_thumbnail_url($prevPhoto->ID); ?>" alt="Photo précédente">
+                    <?php endif; ?>
+                    <?php if ($nextPhoto) : ?>
+                        <img class="next-image" src="<?php echo get_the_post_thumbnail_url($nextPhoto->ID); ?>" alt="Photo suivante">
+                    <?php endif; ?>
+                </div>
 
-            <div class="single-photo-nav-preview">
-                <?php if ($prevPhoto) : ?>
-                    <img class="previous-image " src="<?php echo get_the_post_thumbnail_url($prevPhoto->ID); ?>" alt="Photo précédente">
-                <?php endif; ?>
-                <?php if ($nextPhoto) : ?>
-                    <img class="next-image" src="<?php echo get_the_post_thumbnail_url($nextPhoto->ID); ?>" alt="Photo suivante">
-                <?php endif; ?>
+                <div class="single-photo-nav">
+                    <?php
+
+                    if ($prevPhoto) : ?>
+                        <a class="single-prev-photo-arrow" href="<?php echo get_permalink($prevPhoto->ID); ?>">
+                            <img src="<?php echo get_template_directory_uri() . '/assets/images/left-arrow.png'; ?>" alt="Previous Photo" />
+                        </a>
+                    <?php endif; ?>
+                    <?php if ($nextPhoto) : ?>
+                        <a class="single-next-photo-arrow" href="<?php echo get_permalink($nextPhoto->ID); ?>">
+                            <img src="<?php echo get_template_directory_uri() . '/assets/images/right-arrow.png'; ?>" alt="Next Photo" />
+                        </a>
+                    <?php endif; ?>
+                </div>
+                </div>
             </div>
         </div>
     </section>
