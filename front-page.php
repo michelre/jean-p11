8d504d511aed3444f2c8e453ea9ec5affd164737
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-29 13:49:53 +0200

    Dropdown filters dynamic content functionality

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <?php get_header() ?>
 <img class="banner-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner.png" alt="Photograph event">
 
+
 <section class="filter-section">
     <div class="left-filter-section">
         <div class="dropdown">
@@ -9,11 +10,14 @@
                 <img class="chevron categories-chevron" src="<?php echo get_template_directory_uri(); ?>/assets/images/chevron-down.png?>" alt="">
             </div>
             <ul class="dropDownList categories-list ">
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
+                <?php $categories = get_terms(array(
+                    'taxonomy' => 'category',
+                    'orderby' => 'name'
+                ));
+                foreach ($categories as $category) {
+                    echo '<li class="dropDownListItem ' . $category->name . 'ListItem " ><span class="list-padding">' . $category->name . '</li>';
+                }
+                ?>
             </ul>
         </div>
         <div class="dropdown">
@@ -22,16 +26,19 @@
                 <img class="chevron formats-chevron" src="<?php echo get_template_directory_uri(); ?>/assets/images/chevron-down.png?>" alt="">
             </div>
             <ul class="dropDownList formats-list">
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
-                <li class="dropDownListItem"><span class="list-padding">ubiuzece</span></li>
+                <?php $formats = get_terms(array(
+                    'taxonomy' => 'format',
+                    'orderby' => 'name'
+                ));
+                foreach ($formats as $format) {
+                    echo '<li class="dropDownListItem ' . $format->name . 'ListItem " ><span class="list-padding">' . $format->name . '</li>';
+                }
+                ?>
             </ul>
         </div>
     </div>
-    <div class="right-filter-section" >
-    <div class="dropdown">
+    <div class="right-filter-section">
+        <div class="dropdown">
             <div class="dropDownBtn sort-btn">
                 <p class="dropDownBtntext">Trier par</p>
                 <img class="chevron sort-chevron" src="<?php echo get_template_directory_uri(); ?>/assets/images/chevron-down.png?>" alt="">
@@ -44,23 +51,23 @@
     </div>
 </section>
 <section class="gallery-section">
-        <?php
-        $galleryPhotos = new WP_Query(
-            array(
-                'post_type' => 'photo',
-                'posts_per_page' => 12,
-                'orderby' => 'date',
-                'order' => 'DESC',
-            )
-        );
-        while ($galleryPhotos->have_posts()) : $galleryPhotos->the_post();
-            get_template_part('template-parts/photo-card');
+    <?php
+    $galleryPhotos = new WP_Query(
+        array(
+            'post_type' => 'photo',
+            'posts_per_page' => 12,
+            'orderby' => 'date',
+            'order' => 'DESC',
+        )
+    );
+    while ($galleryPhotos->have_posts()) : $galleryPhotos->the_post();
+        get_template_part('template-parts/photo-card');
 
-        endwhile;
-        wp_reset_postdata();
-        ?>
-                <div class="mota-button load-more-btn">Charger plus</div>     
-    </section>
+    endwhile;
+    wp_reset_postdata();
+    ?>
+    <div class="mota-button load-more-btn">Charger plus</div>
+</section>
 </main>
 
 <?php get_footer() ?>
